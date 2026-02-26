223b892892661ea6ef43b214d09bb8d634b4aa2c
Quadrata <quadrata8902@gmail.com>
2026-02-26 15:47:04 +0800

    新增單位圓判斷函數

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// 這個函數接收一個座標點，回傳 true 或 false
+fn is_on_unit_circle(point: (i32, i32)) -> bool {
+    let (x, y) = point;
+    // 判斷 x平方 + y平方 是否等於 1
+    x * x + y * y == 1
+}
+
 fn main() {
-    println!("Hello, world!");
+    let test_point = (0, 1);
+    let result = is_on_unit_circle(test_point);
+    
+    println!("點 (0, 1) 在單位圓上嗎？ {}", result);
 }
