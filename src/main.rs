bb35c5ca0e2b3b1b1e8828c95311222cf63ea180
Quadrata <quadrata8902@gmail.com>
2026-03-02 20:08:53 +0800

    加入了struct這個構造

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
-// 這個函數接收一個座標點，回傳 true 或 false
-fn is_on_unit_circle(point: (i32, i32)) -> bool {
-    let (x, y) = point;
-    // 判斷 x平方 + y平方 是否等於 1
-    x * x + y * y == 1
+// 1. 定義我們專屬的數學結構
+// 這就像在宣告：令 Point 為一個包含整數 x 與整數 y 的空間元素
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+// 2. 改寫函數：現在定義域的型別不是 (i32, i32) 了，而是我們自己發明的 Point！
+fn is_on_unit_circle(p: Point) -> bool {
+    // 在 struct 中，我們使用「點 (.)」來取得裡面的特定維度數值
+    p.x * p.x + p.y * p.y == 1
 }
 
 fn main() {
-    let test_point = (0, 1);
+    // 3. 實例化 (Instantiation)：創造一個具體的點
+    // 寫法比 Tuple 明確很多，直接標明 x 是 0，y 是 1
+    let test_point = Point { x: 0, y: 1 };
+    
+    // 4. 把這個自訂結構丟進函數裡
     let result = is_on_unit_circle(test_point);
     
-    println!("點 (0, 1) 在單位圓上嗎？ {}", result);
+    println!("點在單位圓上嗎？ {}", result);
 }
