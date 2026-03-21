0e1f1b757e99b5a00b35334290897b07dccf4426
Quadrata <quadrata8902@gmail.com>
2026-03-21 16:19:45 +0800

    實作finite field上的運算，包含加減乘除、次方

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ impl FieldElement{
 
     fn add(&self, other: &FieldElement) -> FieldElement{
     if self.prime != other.prime {
-        panic!("Different Field Elements");
+        panic!("!different field elements");
     }
     let new_value=(self.value + other.value) % self.prime;
     FieldElement::new(new_value, self.prime)
@@ -25,28 +25,65 @@ impl FieldElement{
     
     fn mul(&self, other: &FieldElement) -> FieldElement {
         if self.prime != other.prime {
-            panic!("Different Field Elements");
+            panic!("!different field elements");
         }
         let new_value = (self.value * other.value) % self.prime;
         FieldElement::new(new_value, self.prime)
+    }
+
+    fn sub(&self, other: &FieldElement) -> FieldElement {
+        if self.prime != other.prime {
+            panic!("!different field elements");
+        }
+        let new_value = (self.value + self.prime - other.value) % self.prime;
+        FieldElement::new(new_value, self.prime)
+    }
+
+    fn pow(&self, exponent: u64) -> FieldElement {
+        let mut result = 1;
+        let mut base = self.value;
+        let mut exp = exponent;
+
+        while exp > 0 {
+            if exp % 2 == 1 {
+                result = (result * base) % self.prime;
+            }
+            base = (base * base) % self.prime;
+            exp /= 2;
+        }
+        FieldElement::new(result, self.prime)
+    }
+    fn inv(&self) -> FieldElement {
+        if self.value == 0 {
+            panic!("!no iverse for zero");
+        }
+        self.pow(self.prime - 2)
+    }
+    fn div(&self, other: &FieldElement) -> FieldElement {
+        let other_inv = other.inv();
+        self.mul(&other_inv)
     }
 }
 
 fn main () {
     let p = 17;
-    let a = FieldElement::new(10,p);
-    let b = FieldElement::new(15,p);
+    let a = FieldElement::new(10, p);
+    let b = FieldElement::new(15, p);
 
-    print!("Element A:");
+    print!("Element A: ");
     a.print();
-    print!("Element B:");
+    print!("Element B: ");
     b.print();
 
-    let c = a.add(&b);
-    print!("A+B=");
-    c.print();
+    let e = a.sub(&b);
+    print!("A - B = ");
+    e.print();
 
-    let d = a.mul(&b);
-    print!("A*B=");
-    d.print();
+    let f = a.div(&b);
+    print!("A / B = ");
+    f.print();
+
+    let verify = f.mul(&b);
+    print!("(A / B) * B = ");
+    verify.print();
 }
